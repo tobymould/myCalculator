30fce45ed41aa2ac41874e53e475623559aba58a
tobymould <toby.mould@icloud.com>
2020-08-05 13:59:39 +0100

    rewrote the logic entirely - still some tweeking to do to get functional

--- app.js
+++ app.js
@@ -1,85 +1,156 @@
 const inputs = document.querySelector('form');
 const submit = document.querySelector('.submit');
 const valueInDisplay = document.querySelector('#display h2');
-console.log(valueInDisplay);
 
-// instantiate your variables:
-let result = '';
+// instantiate  variables:
+// let result = '';
+let array = [];
+let string = '';
+// let operator;
+// let value2;
 let runningTotal = 0;
+const operators = ['+', '-', '*', ' /'];
 
-const resetInput = () => {
-  runningTotal = 0;
-  valueInDisplay.innerText = runningTotal;
-};
+// const resetInput = () => {
+//   runningTotal = 0;
+//   valueInDisplay.innerText = runningTotal;
+// };
 
 // ----------STEP 1: GATHER THE INPUT---------- //
 const inputGather = event => {
   event.preventDefault();
   const input = event.target.value;
-  console.log(`The input Array currently is ${result}`);
-  console.log(`The value just clicked is ${event.target.value}`);
-  return (result += input);
+  // console.log(input);
+
+  if (!input) {
+    return;
+  } else if (input === '=') {
+    turnArrayIntoMath();
+  } else {
+    array.push(input);
+    // console.log(array);
+  }
+
+  // if (value1 && operator) {
+  // if (typeof input === 'number' && isFinite(input)) {
+  //   return (value1 = input);
+  // } else if (input.includes('+')) {
+  //   return (operator = '+');
+  // } else if (input.includes('-')) {
+  //   return (operator = '-');
+  // } else if (input.includes('*')) {
+  //   return (operator = '*');
+  // } else if (input.includes('/')) {
+  //   return (operator = '/');
+  // }
+  // // }
+
+  // console.log(value1);
+  // console.log(operator);
+  // console.log(runningTotal);
+
+  // calculation(value1, operator);
+  // console.log(`The input Array currently is ${result}`);
+  // console.log(`The value just clicked is ${event.target.value}`);
+  // return (result += input);
 };
 
 // ----------STEP 2: SEPARATE INTO SEPERATE NUMBERS AND OPERATOR ---------- //
-let operator;
-// let splitPosition;
-const seperateInput = () => {
-  if (result.includes('+')) {
-    operator = '+';
-    return result.split('+');
-  }
-  // } else if (result.includes('-')) {
-  //   operator = '-';
-  //   return result.split('-');
-  // } else if (result.includes('*')) {
-  //   operator = '*';
-  //   return result.split('*');
-  // } else if (result.includes('/')) {
-  //   operator = '/';
-  //   return result.split('/');
+const turnArrayIntoMath = () => {
+  let operatorIndex = [];
+  let numberArray = [];
+  let initialIndex = 0;
+
+  array.forEach((element, index) => {
+    if (operators.includes(element)) {
+      operatorIndex.push({ element: element, index: index });
+    }
+  });
+
+  console.log('operatorIndex: ', operatorIndex);
+  // console.log(operatorIndex[0].element);
+  // console.log(operatorIndex[0].index);
+
+  const lengthOfInput = array.length;
+  console.log('lengthOfInput', lengthOfInput);
+  console.log('Array before: ', array);
+
+  operatorIndex.map((object, index) => {
+    console.log(`Round ${index}`, 'initialIndex:', initialIndex, 'object.index:', object.index);
+
+    if (index === 0) {
+      let result = array.slice(0, object.index);
+      console.log('result', result);
+      numberArray.push(result);
+      initialIndex = object.index + 1;
+
+      const secondnumber = operatorIndex[1].index;
+
+      console.log(`Round ${index} - part 2: `, 'initialIndex:', initialIndex, 'operatorIndex[1].index:', secondnumber);
+      console.log('secondnumber', secondnumber);
+      result = array.slice(initialIndex, secondnumber);
+      console.log('result', result);
+      numberArray.push(result);
+    } else if (index === 1) {
+      return;
+    } else {
+      let result = array.slice(initialIndex, object.index);
+      console.log('result', result);
+      numberArray.push(result);
+      initialIndex = object.index;
+      console.log(initialIndex + 1);
+    }
+  });
+
+  console.log('numbers array', numberArray);
+  // if (typeof element === 'number' && isFinite(input)) {
+  //   parseFloat(element);
   // }
+
+  // if (!array.includes('+')) {
+  //   console.log(array);
+  //   array.join('+');
+  // }
+
+  // if (string.includes('-')) {
+  //   string.split('-');
+  // }
+  // if (string.includes('*')) {
+  //   string.split('*');
+  // }
+  // if (string.includes('/')) {
+  //   string.split('/');
+  // }
+  console.log('operatorIndex', operatorIndex);
+  // console.log('string of Array: ', array);
 };
-console.log(`This is the input split apart inside an array: ${result}`);
-console.log(operator);
+// console.log(value1);
+// console.log(operator);
 
 // -------Step 3: Calculate the result by adding Array element [0] to [1]-------//
-const final = (result, operator) => {
-  const finalValue = result[0] + result[1];
-  runningTotal += finalValue;
-  valueInDisplay.innerText = runningTotal;
-  // return (finalValue = `${result[0]} ${operator} ${result[1]} equals ${result[0] + result[1]}`);
-};
+// const calculation = string => {
+//   if (value1 && operator) {
+//     if (operator === '+') {
+//       return runningTotal + value1;
+//     } else if (operator === '-') {
+//       return runningTotal - value1;
+//     } else if (operator === '*') {
+//       return runningTotal * value1;
+//     } else if (operator === '/') {
+//       return runningTotal / value1;
+//     }
+
+//     valueInDisplay.innerText = runningTotal;
+//     // value1 = null;
+//     // operator = null;
+//   }
+// };
 
 //-----------STEP 5: (FUNCTION 1) CREATE ELEMENT & ADD TO HTML----------//
-// const createElement = () => {
-//   const p = document.createElement('p');
-//   p.innerHTML = calculation();
-//   display.append(p);
-// };
 
 // ----------STEP 4: Calculate the Result & present on page---------- //
-let finalValue;
-const calculation = event => {
-  event.preventDefault();
-  const resultArray = seperateInput();
-  console.log(final(resultArray, operator));
-  // result = '';
-  // createElement();
-};
 
-console.log(final);
+// console.log(final);
 
 inputs.addEventListener('click', inputGather);
-submit.addEventListener('click', calculation);
-
-// const createElement = () => {
-//   const p = document.createElement('p');
-//   p.innerHTML = calculation();
-//   display.append(p);
-// };
-
-// else if (result.contains('-')) {
-//   // splitPosition = result.indexOf('-');
-//   operator = '-';
-//   return result.split('-');}
+// submit.addEventListener('click', );
